eee571f4ae68c9eb63a55d31b01e8866adfdfeff
Roger McCoy <rogmccoy@gmail.com>
2019-06-20 11:21:30 -0400

    book route saves to db, image files stored as base64

--- models/book.js
+++ models/book.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 const path = require('path')
-const coverImageBasePath = 'uploads/bookCovers'
+// const coverImageBasePath = 'uploads/bookCovers'
 
 const bookSchema = new mongoose.Schema({
     title: {
@@ -23,10 +23,18 @@ const bookSchema = new mongoose.Schema({
         required: true,
         default: Date.now
     },
-    coverImageName: {
+    coverImage: {
+        type: Buffer,
+        required: true
+    },
+    coverImageType: {
+        type: String,
+        required: true
+    },
+    /* coverImageName: {
         type: String,
         required: false
-    },
+    }, */
     author: {
         type: mongoose.Schema.Types.ObjectId,
         required: true,
@@ -34,13 +42,20 @@ const bookSchema = new mongoose.Schema({
     }
 })
 
-// this creates a virtual property in the book schema, in this case: coverImagePath
+/* // this creates a virtual property in the book schema, in this case: coverImagePath
 bookSchema.virtual('coverImagePath').get(function() {
     if( this.coverImageName != null) {
         return path.join('/', coverImageBasePath, this.coverImageName)
+    }
+}) */
+
+// this is converting the base64 encoded data back to an image file used by the HTML page
+bookSchema.virtual('coverImagePath').get(function() {
+    if (this.coverImage != null && this.coverImageType != null) {
+        return `data:${this.coverImageType};charset=utf-8;base64,${this.coverImage.toString('base64')}`
     }
 })
 
 module.exports = mongoose.model('Book', bookSchema)
 // coverImageBasePath will be a property on the Book object created from this model
-module.exports.coverImageBasePath = coverImageBasePath
+// module.exports.coverImageBasePath = coverImageBasePath
--- routes/books.js
+++ routes/books.js
@@ -1,18 +1,19 @@
 const express = require('express')
 const router = express.Router()
-const multer = require('multer')
-const path = require('path')
-const fs = require('fs')
+// const multer = require('multer')
+// const path = require('path')
+// const fs = require('fs')
 const Author = require('../models/author')
 const Book = require('../models/book')
-const uploadPath = path.join('public', Book.coverImageBasePath)
+// const uploadPath = path.join('public', Book.coverImageBasePath)
 const imageMimeTypes = ['image/jpeg', 'image/png', 'image/gif']
-const upload = multer({
+// multer is used to upload files and store on the file system
+/* const upload = multer({
     dest: uploadPath,
     fileFilter: (req, file, callback) => {
         callback(null, imageMimeTypes.includes(file.mimetype))
     }
-})
+}) */
 
 // all books route
 router.get('/', async (req, res) => {
@@ -45,34 +46,37 @@ router.get('/new', (req, res) => {
 })
 
 // create book route
-router.post('/', upload.single('cover'), async (req, res) => {
+// use the upload.single() when you are uploading an actual image file
+// router.post('/', upload.single('cover'), async (req, res) => {
+router.post('/', async (req, res) => {
 
-    const fileName = req.file != null ? req.file.filename : null
+    // const fileName = req.file != null ? req.file.filename : null
     
     const book = new Book({
         title: req.body.title,
         author: req.body.author,
         publishDate: new Date(req.body.publishDate),
         pageCount: req.body.pageCount,
-        coverImageName: fileName,
+        // coverImageName: fileName,
         description: req.body.description
     })
+    saveCover(book, req.body.cover)
 
     try {
         const newBook = await book.save()
         // res.redirect(`books/${newBook.id}`)
         res.redirect(`books`)
     } catch {
-        if (book.coverImageName != null) removeBookCover(book.coverImageName)
+        // if (book.coverImageName != null) removeBookCover(book.coverImageName)
         renderNewPage(res, book, true)
     }
 })
 
-function removeBookCover(fileName) {
+/* function removeBookCover(fileName) {
     fs.unlink(path.join(uploadPath, fileName), err => {
         if (err) console.error(err)
     })
-}
+} */
 
 async function renderNewPage(res, book, hasError = false) {
     try {
@@ -85,6 +89,18 @@ async function renderNewPage(res, book, hasError = false) {
         res.render('books/new', params)
     } catch {
         res.redirect('/books')
+    }
+}
+
+// coverEncoded is the image file being uploaded
+// this function is storing the uploaded image file as a base64 encoded string
+function saveCover(book, coverEncoded) {
+    if (coverEncoded == null) return
+    // convert the coverEncoded image into JSON data
+    const cover = JSON.parse(coverEncoded)
+    if (cover != null && imageMimeTypes.includes(cover.type)) {        
+        book.coverImage = new Buffer.from(cover.data, 'base64')
+        book.coverImageType = cover.type
     }
 }
 
